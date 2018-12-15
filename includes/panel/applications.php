ac9a440fe65faf3f21f0a1eccdd201b931361eed
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-15 15:53:18 +0100

    test

--- includes/panel/applications.php
+++ includes/panel/applications.php
@@ -8,16 +8,12 @@ function applications_list($user_id)
 
     foreach ($user_applications as $user_application) {
         $client = sql_select('clients', 'redirect_url,user_id,name,logo_url,description,suspended', "client_id='{$user_application}'", true);
-        $owner = sql_select('users', 'first_name,last_name', "id='{$client['user_id']}'", true);
+        $owner = sql_select('users', 'username', "id='{$client['user_id']}'", true);
 
         echo <<<HTML
         <li class="collection-item avatar">
-            <img class="circle" src="https://{$allowed_domain}/favicon.ico" onerror="this.src='https://betaplus.ams3.cdn.digitaloceanspaces.com/luca/images/not_found.png'"> <span class="title">{$domain_title}</span>
-            <p>{$allowed_domain}</p>
-            <p>
-                <a href="?goto={$allowed_domain}&CSRFtoken={$CSRFtoken}" class="btn-small waves-effect waves-light orange" target="_blank">Open</a>
-            </p>
-            <a class="secondary-content" href="?delete={$allowed_domain}&CSRFtoken={$CSRFtoken}" onclick="return confirm('Are you sure?')"><i class="material-icons orange-text">delete</i></a>
+            <img class="circle" src="{$client['logo_url']}" onerror="this.src='https://github.com/identicons/{$user_application}.png'"> <span class="title">{$client['name']}</span>
+            <p>Owned by <span class="blue-text">{$owner['username']}</span></p>
         </li>
 HTML;
     }
