8fe168d2ee3ac93af0605631c5508f9a3b8e41cb
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-15 16:31:22 +0100

    update

--- includes/panel/applications.php
+++ includes/panel/applications.php
@@ -29,12 +29,43 @@ function application_info($client_id)
     $client = clean_data($client_id);
     $client = sql_select('clients', 'redirect_url,user_id,name,logo_url,description,suspended', "client_id='{$client_id}'", true);
 
+    $CSRFtoken = csrf_gen();
+
     echo <<<HTML
-    <h4>{$client['name']}</h4>
+    <div class="row">
+        <div class="col s4">
+            <img class="responsive-img" src="{$client['logo_url']}" onerror="this.src='https://github.com/identicons/{$client_id}.png'" width="75">
+        </div>
+        <div class="col s8">
+            <h4>
+                {$client['name']}
+            </h4>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col s8">
+            <h4>
+                Permissions
+            </h4>
+        </div>
+        <div class="col s4">
+            <a href="?revoke={$client_id}&CSRFtoken={$CSRFtoken}" class="btn waves-effect blue accent-4" onclick="return confirm('Are you sure?')">Revoke Access</a>
+        </div>
+    </div>
+    <div class="row">
+        <ul class="browser-default">
+            <li>get scopes</li>
+            <li>query scopes</li>
+            <li>display description</li>
+        </ul>
+    </div>
 HTML;
 }
 
 function application_revoke($client_id, $CSRFtoken)
 {
-    //
+    //remove app from user
+    //delete all authorization codes with client_id
+    //delete all access_tokens with client_id
 }
