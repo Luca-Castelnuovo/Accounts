62a901551ca44b0fd1921954e9701fcd02cb6a1f
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-16 22:39:23 +0100

    update

--- includes/init.php
+++ includes/init.php
@@ -10,6 +10,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/sql.php';
 
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/panel/applications.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/panel/authentication.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/includes/panel/clients.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/panel/security.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/panel/template.php';
 
---
+++ includes/panel/clients.php
@@ -0,0 +1,7 @@
+<?php
+
+//function to create client
+
+//function to update client
+
+//function to delete client
