c4f317b67e6f48388bb5a4b9b19cbede60b3d01b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-17 20:29:37 +0100

    update

--- application.php
+++ application.php
@@ -6,12 +6,12 @@ loggedin();
 
 page_header('Application');
 
-if (!isset($_GET['id']) || empty($_GET['id'])) {
-    redirect('/home');
-}
-
 if (isset($_GET['revoke']) && !empty($_GET['revoke']) && isset($_GET['CSRFtoken']) && !empty($_GET['CSRFtoken'])) {
     application_revoke($_SESSION['id'], $_GET['revoke'], $_GET['CSRFtoken']);
+}
+
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    redirect('/home');
 }
 
 ?>
