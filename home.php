d49202bb6c61ffa3d96cb4969d616919404eab88
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-17 21:46:53 +0100

    update

--- home.php
+++ home.php
@@ -8,6 +8,9 @@ page_header('Home');
 
 ?>
 
+<style>
+    .mt-1_52rem{margin-top:1.52rem}
+</style>
 <div class="row">
     <h4>Authorized Apps</h4>
     <?= applications_list($_SESSION['id']); ?>
@@ -18,7 +21,7 @@ page_header('Home');
             <h4>Your clients</h4>
         </div>
         <div class="col s12 m3">
-            <a href="/client/add" class="btn waves-effect blue accent-4">Create client</a>
+            <a href="/client/add" class="btn waves-effect blue accent-4 mt-1_52rem">Create client</a>
         </div>
     </div>
     <?= clients_list($_SESSION['id']); ?>
