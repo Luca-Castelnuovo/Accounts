4c7db746a3beab4cef0add61eade471dfcb8f195
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-17 21:31:29 +0100

    update

--- client.php
+++ client.php
@@ -7,11 +7,15 @@ loggedin();
 page_header('Client');
 
 if (isset($_GET['delete']) && !empty($_GET['delete']) && isset($_GET['CSRFtoken']) && !empty($_GET['CSRFtoken'])) {
-    client_delete($_SESSION['id'], $_GET['delete'], $_GET['CSRFtoken']);
+    client_delete($_GET['CSRFtoken'], $_SESSION['id'], $_GET['delete']);
 }
 
 if (isset($_GET['reset']) && !empty($_GET['reset']) && isset($_GET['CSRFtoken']) && !empty($_GET['CSRFtoken'])) {
-    client_reset($_SESSION['id'], $_GET['reset'], $_GET['CSRFtoken']);
+    client_reset($_GET['CSRFtoken'], $_SESSION['id'], $_GET['reset']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    client_update($_POST['CSRFtoken'], $_SESSION['id'], $_POST['client_id'], $_POST['logo_url'], $_POST['name'], $_POST['description'], $_POST['redirect_uri']);
 }
 
 if (!isset($_GET['id']) || empty($_GET['id'])) {
--- includes/panel/clients.php
+++ includes/panel/clients.php
@@ -65,6 +65,7 @@ function client_info($user_id, $client_id)
                 </div>
             </div>
         </div>
+        <hr>
         <form method="post">
             <div class="row">
                 <div class="input-field col s12">
@@ -92,6 +93,7 @@ function client_info($user_id, $client_id)
             </div>
 
             <div class="row">
+                <input type="hidden" name="client_id" value="{$client['client_id']}"/>
                 <input type="hidden" name="CSRFtoken" value="{$CSRFtoken}"/>
                 <div class="col s12 m8">
                     <button class="col s12 btn-large waves-effect blue accent-4" type="submit">Update Client</button>
@@ -109,27 +111,71 @@ HTML;
 }
 
 
-function client_delete($user_id, $client_id, $CSRFtoken)
+function client_delete($CSRFtoken, $user_id, $client_id)
 {
     csrf_val($CSRFtoken);
 
     $user_id = clean_data($user_id);
     $client_id = clean_data($client_id);
 
-    //delete client and all codes,tokens associated with id
+    $client = sql_select('clients', 'user_id', "client_id='{$client_id}'", true);
+
+    if ($client['user_id'] != $user_id) {
+        redirect('/home');
+    }
+
+    sql_delete('clients', "client_id='{$client_id}'");
+    sql_delete('access_tokens', "client_id='{$client_id}'");
+    sql_delete('authorization_codes', "client_id='{$client_id}'");
 
     redirect('/home', 'Client deleted.');
 }
 
 
-function client_reset($user_id, $client_id, $CSRFtoken)
+function client_reset($CSRFtoken, $user_id, $client_id)
 {
     csrf_val($CSRFtoken);
 
     $user_id = clean_data($user_id);
     $client_id = clean_data($client_id);
 
-    //reset secret
+    $client = sql_select('clients', 'user_id', "client_id='{$client_id}'", true);
+
+    if ($client['user_id'] != $user_id) {
+        redirect('/home');
+    }
+
+    $client_secret = gen(64);
+
+    sql_update('clients', ['client_secret' => $client_secret], "client_id='{$client_id}'");
 
     redirect('/client?id=' . $client_id, 'Client secret reset.');
+}
+
+//create client
+
+function client_update($CSRFtoken, $user_id, $client_id, $logo_url, $name, $description, $redirect_uri)
+{
+    csrf_val($CSRFtoken);
+
+    $user_id = clean_data($user_id);
+    $client_id = clean_data($client_id);
+
+    $client = sql_select('clients', 'user_id', "client_id='{$client_id}'", true);
+
+    if ($client['user_id'] != $user_id) {
+        redirect('/home');
+    }
+
+    $logo_url = clean_data($logo_url);
+    $name = clean_data($name);
+    $description = clean_data($description);
+    $redirect_uri = clean_data($redirect_uri);
+
+    sql_update('clients', [
+        'logo_url' => $logo_url,
+        'name' => $name,
+        'description' => $description,
+        'redirect_uri' => $redirect_uri,
+    ], "client_id='{$client_id}'");
 }
