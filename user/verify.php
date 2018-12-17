cb0c97f8963307c6b8fb15128a72a99e214247ca
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-17 17:48:55 +0100

    update

--- user/verify.php
+++ user/verify.php
@@ -2,9 +2,9 @@
 
 require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
 
-$token = check_data($_REQUEST['token'], true, 'Token', true, true, '/');
+$token_GET = check_data($_REQUEST['token'], true, 'Token', true, true, '/');
 
-$token = sql_select('general_tokens', 'revoked,expires,user_id', "token='{$token}' AND type='verify_email'", true);
+$token = sql_select('general_tokens', 'revoked,expires,user_id', "token='{$token_GET}' AND type='verify_email'", true);
 
 if ($token['expires'] <= time() || $token['revoked']) {
     redirect('/', 'Link already used or expired.');
@@ -13,5 +13,9 @@ if ($token['expires'] <= time() || $token['revoked']) {
 sql_update('users', [
     'email_verified' => '1'
 ], "username='{$token['user_id']}'");
+
+sql_update('general_tokens', [
+    'revoked' => '1'
+], "token='{$token_GET}'");
 
 redirect('/', 'Your account is verified.');
