217c840ec3b476823492cfb139d4f37cac029f1b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-21 15:06:48 +0100

    update

--- admin/users.php
+++ admin/users.php
@@ -4,6 +4,28 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
 
 loggedin_admin();
 
+if (isset($_GET['CSRFtoken']) && isset($_GET['id']) && isset($_GET['type']) && isset($_GET['status'])) {
+    $id = check_data($_GET['id'], false, '', true);
+    $status = check_data($_GET['status'], false, '', true);
+    switch ($_GET['type']) {
+        case 'verified':
+            sql_update('users', [
+                'email_verified' => $status
+            ], "username='{$id}'");
+            break;
+        case 'developer':
+            sql_update('users', [
+                'developer' => $status
+            ], "username='{$id}'");
+            break;
+        case 'admin':
+            sql_update('users', [
+                'admin' => $status
+            ], "username='{$id}'");
+            break;
+}
+}
+
 page_header('Users');
 
 ?>
@@ -23,6 +45,55 @@ page_header('Users');
         </thead>
 
         <tbody>
+            <?php
+            $users = sql_select('users', 'id,username,email,email_verified,picture_url,created,applications,developer,admin', "true", true);
+
+            while ($user = $users->fetch_assoc()) {
+                $CSRFtoken = csrf_gen();
+
+                if ($user['email_verified']) {
+                    $verified_class = 'green';
+                    $verified_text = 'True';
+                    $verified_toggle = 'false';
+                } else {
+                    $verified_class = 'red';
+                    $verified_text = 'False';
+                    $verified_toggle = 'true';
+                }
+
+                if ($user['developer']) {
+                    $developer_text = 'green';
+                    $developer_class = 'True';
+                    $developer_toggle = 'false';
+                } else {
+                    $developer_text = 'red';
+                    $developer_class = 'False';
+                    $developer_toggle = 'true';
+                }
+
+                if ($user['admin']) {
+                    $admin_class = 'green';
+                    $admin_text = 'True';
+                    $admin_toggle = 'false';
+                } else {
+                    $admin_class = 'red';
+                    $admin_text = 'False';
+                    $admin_toggle = 'true';
+                }
+
+                echo <<<HTML
+                <tr id="{$user['id']}">
+                    <td><img src="{$user['picture_url']}" class="responsive-img" width="100"></td>
+                    <td>{$user['username']}</td>
+                    <td><a href="mailto:{$user['email']}">{$user['email']}</a></td>
+                    <td><a href="/admin/users?CSRFtoken={$CSRFtoken}&id={$user['id']}&type=verified&status={$verified_toggle}" class="btn waves-effect {$verified_class} accent-4">{$verified_text}</a></td>
+                    <td><a href="/admin/users?CSRFtoken={$CSRFtoken}&id={$user['id']}&type=developer&status={$developer_toggle}" class="btn waves-effect {$developer_class} accent-4">{$developer_text}</a></td>
+                    <td><a href="/admin/users?CSRFtoken={$CSRFtoken}&id={$user['id']}&type=admin&status={$admin_toggle}" class="btn waves-effect {$admin_class} accent-4">{$admin_text}</a></td>
+                    <td>{$user['created']}</td>
+                </tr>
+HTML;
+            }
+            ?>
             <tr id="12">
                 <td><img src="https://avatars3.githubusercontent.com/u/26206253" class="responsive-img" width="100"></td>
                 <td>ltcastelnuovo</td>
