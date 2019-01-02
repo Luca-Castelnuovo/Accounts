c5b6c4a43449d9ea15a2f01df421da88893561fc
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-02 17:36:28 +0100

    in the process of #39

---
+++ demo.php
@@ -0,0 +1,27 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
+
+loggedin_developer();
+
+$token = request(
+    'https://api.lucacastelnuovo.nl/mail/',
+    [
+        "grant_type" => "client_credentials",
+        "client_id" => "{$GLOBALS['config']->client_id}",
+        "client_secret" => "{$GLOBALS['config']->client_secret}"
+    ]
+);
+
+print_r($token);
+
+print_r(request(
+    'https://api.lucacastelnuovo.nl/mail/',
+    [
+        "access_token" => "{$token['access_token']}",
+        "to" => "{$to}",
+        "subject" => "{$subject}",
+        "body" => "{$body}",
+        "from_name" => "LTC Auth"
+    ]
+));
